0a122e7a0938d1b34df319b185760fc8506e2ef8
sososos <youamg2@126.com>
2019-05-16 01:00:42 +0800

    add window2 trained txt to compute similar & 6race txt test

--- race_test/train.sh
+++ race_test/train.sh
@@ -1,2 +1,2 @@
 #train_file=$1
-../bin/word2vec -train ../based_data/paticipant_train/combineout_userf_train.txt -output ../data/race_partake_vec_6.txt -size 100 -window 6 -sample 1e-3-negative 5 -hs 0 -binary 0 -cbow 1 -iter 100 -save-vocab ../data/race_partake_vocab_6.txt
+../bin/word2vec -train ../based_data/paticipant_train/combineout_userf_train.txt -output ../data/race_partake_vec_2.txt -size 100 -window 2 -sample 1e-3-negative 5 -hs 0 -binary 0 -cbow 1 -iter 100 -save-vocab ../data/race_partake_vocab_2.txt

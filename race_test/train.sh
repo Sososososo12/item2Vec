6d184c60201f81f3cc52664a2aa297a96dcdc9d9
sososos <youamg2@126.com>
2019-06-20 16:16:03 +0800

    add 20 dimension

--- race_test/train.sh
+++ race_test/train.sh
@@ -1,2 +1,2 @@
 #train_file=$1
-../bin/word2vec -train ../based_data/paticipant_train/combineout_userf_train.txt -output ../data/race_partake_vec_2.txt -size 100 -window 2 -sample 1e-3-negative 5 -hs 0 -binary 0 -cbow 1 -iter 100 -save-vocab ../data/race_partake_vocab_2.txt
+../bin/word2vec -train ../based_data/paticipant_train/combineout_userf_train.txt -output ../data/race_partake_vec_20dem.txt -size 20 -window 2 -sample 1e-3-negative 5 -hs 0 -binary 0 -cbow 1 -iter 100 -save-vocab ../data/race_partake_vocab_20dem.txt
